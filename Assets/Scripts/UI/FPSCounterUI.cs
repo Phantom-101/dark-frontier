81d23a249838a5085422bd0038e791e1dcb8e8dc
Phantom-101 <ccradical2468@gmail.com>
2021-03-02 09:43:24 -0800

    added fps counter, lowered fast launcher rate of fire

---
+++ Assets/Scripts/UI/FPSCounterUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounterUI : MonoBehaviour {
+
+    [SerializeField] private Text _text;
+
+    private void Update () {
+
+        _text.text = $"FPS: {(int) (1 / Time.smoothDeltaTime)}";
+
+    }
+
+}
