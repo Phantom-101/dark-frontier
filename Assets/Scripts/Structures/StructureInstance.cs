d6cbb20aa0ed4d9083894030e8cf6d56d1e1dc9b
wertyrt <sol.fentress@gmail.com>
2021-05-14 15:36:40 -0700

    AAAAAAGH
    E

---
+++ Assets/Scripts/Structures/StructureInstance.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureInstance : IStructure, ISerializable<IStructure> {
+    public string Id {
+        get;
+        private set;
+    }
+    public string Name {
+        get;
+        set;
+    }
+    public string Description {
+        get;
+        set;
+    }
+    public IEntity Controller {
+        get;
+        set;
+    }
+    public float Hitpoints {
+        get;
+        private set;
+    }
+    public StatInstance SignatureSize {
+        get;
+        private set;
+    }
+    public StatInstance MaxHitpoints {
+        get;
+        private set;
+    }
+    public List<IStructureSegment> Segments {
+        get;
+        private set;
+    }
+
+    public event OnDestroyedEventHandler OnDestroyed;
+
+    public StructureInstance (InterfacedStructureSaveData serialized) {
+        Id = serialized.Id;
+        Name = serialized.Name;
+        Description = serialized.Description;
+        // TODO find controller with id
+        Hitpoints = serialized.Hitpoints;
+        SignatureSize = StatSaveDataType.Parse (serialized.SignatureSize) as StatInstance;
+        MaxHitpoints = StatSaveDataType.Parse (serialized.MaxHitpoints) as StatInstance;
+        Segments = serialized.Segments.ConvertAll ((e) => StructureSegmentSaveDataType.Parse (e) as IStructureSegment);
+    }
+
+    public ISerialized<IStructure> GetSerialized () { return new InterfacedStructureSaveData (this); }
+
+    public void Initialize () {
+        if (string.IsNullOrWhiteSpace (Id)) Id = Guid.NewGuid ().ToString ();
+        if (SignatureSize == null) SignatureSize = new StatInstance (0, "Signature Size", "The size of the cross section on a scanner.");
+        if (MaxHitpoints == null) MaxHitpoints = new StatInstance (0, "Max Hitpoints", "The maximum number of hitpoints a structure can have.");
+        if (Segments == null) Segments = new List<IStructureSegment> ();
+    }
+
+    public void TakeDamage (float amount, IInfo damager) {
+        Hitpoints = Mathf.Clamp (Hitpoints - amount, 0, MaxHitpoints.AppliedValue);
+        if (Hitpoints == 0) OnDestroyed?.Invoke (this, damager);
+    }
+}
+
---
+++ Assets/Scripts/Structures/StructureSaveData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class InterfacedStructureSaveData : SerializedBase<IStructure> {
+    public string Id;
+    public string Name;
+    public string Description;
+    public string ControllerId;
+    public float Hitpoints;
+    public string SignatureSize;
+    public string MaxHitpoints;
+    public List<string> Segments;
+
+    public InterfacedStructureSaveData (StructureInstance serializable) {
+        DataType = StructureSaveDataType.Default;
+        Id = serializable.Id;
+        Name = serializable.Name;
+        Description = serializable.Description;
+        ControllerId = serializable.Controller.Id;
+        Hitpoints = serializable.Hitpoints;
+        SignatureSize = JsonUtility.ToJson (serializable.SignatureSize);
+        MaxHitpoints = JsonUtility.ToJson (serializable.MaxHitpoints);
+        Segments = serializable.Segments.FindAll ((e) => e is ISerializable<IStructureSegment>).ConvertAll ((e) => JsonUtility.ToJson ((e as ISerializable<IStructureSegment>).GetSerialized ()));
+    }
+
+    public override ISerializable<IStructure> GetSerializable () { return new StructureInstance (this); }
+}
+
