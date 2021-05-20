0817859618e671f86c2dbbebbec6fd068cfba503
Phantom-101 <ccradical2468@gmail.com>
2021-05-20 13:39:56 -0700

    save manager with directory helper methods

---
+++ Assets/Scripts/Files/NewSaveManager.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class NewSaveManager {
+    public static string[] GetUniverseNames () {
+        return Directory.GetDirectories ($"{Application.persistentDataPath}/saves");
+    }
+
+    public static DirectoryInfo[] GetUniverseDirectories () {
+        return GetUniverseNames ().ToList ().ConvertAll (e => GetUniverseDirectory (e)).ToArray ();
+    }
+
+    public static DirectoryInfo GetUniverseDirectory (string name) {
+        return new DirectoryInfo ($"{Application.persistentDataPath}/saves/{name}");
+    }
+
+    public static string[] GetSaveNames (string universe) {
+        return Directory.GetDirectories ($"{Application.persistentDataPath}/saves/{universe}");
+    }
+
+    public static DirectoryInfo[] GetSaveDirectories (string universe) {
+        return GetSaveNames (universe).ToList ().ConvertAll (e => GetSaveDirectory (universe, e)).ToArray ();
+    }
+
+    public static string[] GetSaveNames (DirectoryInfo universe) {
+        return Directory.GetDirectories (universe.FullName);
+    }
+
+    public static DirectoryInfo[] GetSaveDirectories (DirectoryInfo universe) {
+        return GetSaveNames (universe).ToList ().ConvertAll (e => GetSaveDirectory (universe, e)).ToArray ();
+    }
+
+    public static DirectoryInfo GetSaveDirectory (string universe, string name) {
+        return new DirectoryInfo ($"{Application.persistentDataPath}/saves/{universe}/{name}");
+    }
+
+    public static DirectoryInfo GetSaveDirectory (DirectoryInfo universe, string name) {
+        return new DirectoryInfo ($"{universe.FullName}/{name}");
+    }
+
+    public static FileInfo GetStructureFile (string universe, string save) {
+        return new FileInfo ($"{GetSaveDirectory (universe, save)}/structures.txt");
+    }
+
+    public static FileInfo GetStructureFile (DirectoryInfo universe, string save) {
+        return new FileInfo ($"{GetSaveDirectory (universe, save)}/structures.txt");
+    }
+
+    public static FileInfo GetStructureFile (DirectoryInfo save) {
+        return new FileInfo ($"{save.FullName}/structures.txt");
+    }
+
+    public static FileInfo GetFactionFile (string universe, string save) {
+        return new FileInfo ($"{GetSaveDirectory (universe, save)}/factions.txt");
+    }
+
+    public static FileInfo GetFactionFile (DirectoryInfo universe, string save) {
+        return new FileInfo ($"{GetSaveDirectory (universe, save)}/factions.txt");
+    }
+
+    public static FileInfo GetFactionFile (DirectoryInfo save) {
+        return new FileInfo ($"{save.FullName}/factions.txt");
+    }
+
+    public static FileInfo GetSectorFile (string universe, string save) {
+        return new FileInfo ($"{GetSaveDirectory (universe, save)}/sectors.txt");
+    }
+
+    public static FileInfo GetSectorFile (DirectoryInfo universe, string save) {
+        return new FileInfo ($"{GetSaveDirectory (universe, save)}/sectors.txt");
+    }
+
+    public static FileInfo GetSectorFile (DirectoryInfo save) {
+        return new FileInfo ($"{save.FullName}/sectors.txt");
+    }
+}
