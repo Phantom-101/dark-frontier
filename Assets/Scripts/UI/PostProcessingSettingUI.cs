1978d4cf612eb674eb2e9ae53bb5475b088839f8
Phantom-101 <ccradical2468@gmail.com>
2021-05-23 11:38:05 -0700

    disabled opaque texture, added post processing and vsync settings

---
+++ Assets/Scripts/UI/PostProcessingSettingUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PostProcessingSettingUI : MonoBehaviour {
+
+    [SerializeField] private Toggle _toggle;
+    [SerializeField] private bool _on;
+
+    private void Start () {
+
+        _on = PlayerPrefs.HasKey ("PostProcessingEnabled") ? PlayerPrefs.GetInt ("PostProcessingEnabled") == 1 : true;
+        _toggle.isOn = _on;
+
+        _toggle.onValueChanged.AddListener (ChangePostProcessingEnabled);
+
+    }
+
+    void ChangePostProcessingEnabled (bool target) {
+
+        _on = target;
+        PlayerPrefs.SetInt ("PostProcessingEnabled", _on ? 1 : 0);
+
+    }
+
+}
---
+++ Assets/Scripts/UI/VSyncSettingUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VSyncSettingUI : MonoBehaviour {
+    [SerializeField] private VoidEventChannelSO _decreaseVSyncCount;
+    [SerializeField] private VoidEventChannelSO _increaseVSyncCount;
+    [SerializeField] private Text _vSyncText;
+    [SerializeField] private int _vSyncCount;
+
+    private void Start () {
+
+        _vSyncCount = PlayerPrefs.HasKey ("VSyncCount") ? PlayerPrefs.GetInt ("VSyncCount") : 1;
+        _vSyncText.text = _vSyncCount.ToString ();
+
+        _decreaseVSyncCount.OnEventRaised += () => { ChangeVSyncCount (-1); };
+        _increaseVSyncCount.OnEventRaised += () => { ChangeVSyncCount (1); };
+
+    }
+
+    void ChangeVSyncCount (int change) {
+
+        _vSyncCount = Mathf.Clamp (_vSyncCount + change, 0, 4);
+        _vSyncText.text = _vSyncCount.ToString ();
+        QualitySettings.vSyncCount = _vSyncCount;
+        PlayerPrefs.SetInt ("VSyncCount", _vSyncCount);
+
+    }
+}
